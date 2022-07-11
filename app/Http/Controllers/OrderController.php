06b1eaa512dc50b63d02216eaa1c97b72ad8f055
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2022-07-11 10:31:28 +0400

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -31,39 +31,6 @@ class OrderController extends Controller
         }
     }
 
-    <?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\Bond;
-use App\Models\Order;
-use App\Models\OrderPayment;
-use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
-
-class OrderController extends Controller
-{
-    public function store(Request $request){
-        $mytime = Carbon::now();
-        $mytimeConvert  = $mytime->toDateTimeString();
-        $issue_value    = Bond::select('issue_date')->orderBy('issue_date', 'desc')->first();
-        $turnover_value = Bond::select('turnover_date')->orderBy('turnover_date', 'desc')->first();
-
-        $stringConvert  =  (string) $issue_value->issue_date;
-        $stringConvert2 =  (string) $turnover_value->turnover_date;
-
-        if($stringConvert < $mytimeConvert and $stringConvert2 > $mytimeConvert) {
-              $orders = new Order();
-              $orders->bond_number = $request->input('bond_number');
-              $orders->save();
-              return $orders;
-        }
-        else {
-             echo "Alış tarixi Emissiya tarixindən az və Son tədavül tarixindən çox ola bilməz!";
-        }
-    }
-
     public function storePercent(Request $request, int $id) {
         $bonds = Bond::find($id);
         $order_find = Order::find($id);
@@ -99,5 +66,3 @@ class OrderController extends Controller
 
 }
 
-
-}
